e89b19a1b34eff4a2986695761ce09384bacb997
iTUTOR <tutorgaming@gmail.com>
2019-08-20 02:34:26 +0700

    wait button to appear function, opencv matching

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pyautogui
+import cv2 as cv
+import time
+import numpy as np
+from PIL import ImageGrab
 import os
 import time
 current_path = os.path.dirname(os.path.abspath(__file__))
@@ -10,16 +14,69 @@ class ImageButton(object):
     Do the Treasure Hunt
     """
     def __init__(self, button_name):
+        self.button_name = button_name
         self.button_img = asset_path + button_name
+    
+    def locateAllOnScreen(self, path):
+        """
+        Self Implementation on LocateAllOnScreen ! 
+        """
+        screenshot = np.array(ImageGrab.grab())
+        screenshot_gray = cv.cvtColor(screenshot, cv.COLOR_BGR2GRAY)
+        button = cv.imread(path, cv.IMREAD_COLOR)
+        button_gray = cv.cvtColor(button, cv.COLOR_BGR2GRAY)
+        return self.template_match(button_gray,screenshot)
+
+    def template_match(self, finding, original, param=cv.TM_CCOEFF_NORMED):
+        match_method = param
+        templ = finding
+        img = original
+        img_gray = cv.cvtColor(original, cv.COLOR_BGR2GRAY)
         
+        w, h = templ.shape[::-1] 
+
+        res = cv.matchTemplate(img_gray, templ, match_method)
+        
+        # Specify a threshold 
+        threshold = 0.8
+        
+        # Store the coordinates of matched area in a numpy array 
+        loc = np.where( res >= threshold)  
+        
+        # Draw a rectangle around the matched region. 
+        detected_list = zip(*loc[::-1])
+        if not detected_list: 
+            return
+        pt = detected_list[-1]
+        cv.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2) 
+        cv.circle(img,(pt[0] + (w/2) , pt[1] + (h/2)), 2, (0,255,255), 2)
+        
+        # Show the final image with the matched area. 
+        # cv.imshow('Detected',img)
+        yield (pt[0]+(w/2) , pt[1] + (h/2))
 
     def update_location(self):
         # Find this button 
-        button_iterable = pyautogui.locateAllOnScreen(
+        button_iterable = self.locateAllOnScreen(
             self.button_img
         )
         return button_iterable
     
+    def wait_for_button(self, seconds=10):
+        """
+        Fucking Block and Wait For Button until timeout
+        """
+        start = time.time()
+        while True:
+            print "Wait For Button : " + str(self.button_name)
+            result = self.find_button()
+            if result is True:
+                break
+            if time.time() - start >= seconds:
+                result = False 
+                break
+        return result
+
     def find_button(self):
         button_iterable = self.update_location()
         button_findings = list(button_iterable)
@@ -166,8 +223,10 @@ class DoBioGraphyPanAn(object):
         result = do_fight.find_and_click()
         return result
 
+
     def do_skip(self):
         skip = ImageButton("biography_skip.png")
+        skip.wait_for_button(seconds=10)
         result = skip.find_and_click()
         return result
 
@@ -177,27 +236,68 @@ class DoBioGraphyPanAn(object):
         if result:
             self.do_challenge()
             self.do_enter_fight()
-            time.sleep(6)
             # Must Wait For Fight Scene
             self.do_skip()
             # Clear Treasure Screen
             do_treasure_complete = ImageButton("treasure_opened.png")
+            do_treasure_complete.wait_for_button()
             do_treasure_complete.find_and_click()
 
         time.sleep(1)
 
+class GoToErrandLandmark(object):
+    def __init__(self):
+        print "Action : Go To Errand Landmark"
+        self.action() 
+
+    def action(self):
+        go_landmark_errand = ImageButton("landmark_errand.png")
+        result = go_landmark_errand.find_and_click()
+        time.sleep(2)
+
+
+class DoErrand(object):
+    def __init__(self):
+        self.action()
+
+    def action(self):
+        self.do_pay_day()
+        self.do_quick_claim()
+        self.do_confirm()
+
+    def do_pay_day(self):
+        button = ImageButton("errand_pay_day.png")
+        button.wait_for_button(seconds=3)
+        result = button.find_and_click()
+        return result
+
+    def do_quick_claim(self):
+        button = ImageButton("errand_quick_claim.png")
+        button.wait_for_button(seconds=3)
+        result = button.find_and_click()
+        return result
+
+    def do_confirm(self):
+        button = ImageButton("errand_confirm.png")
+        button.wait_for_button(seconds=3)
+        result = button.find_and_click()
+        return result
+
 def main():
-    # Always Start at Battle Tab 
-    go_battle_tab = GoToBattleTabAction()
-    # # Treasure Hunt 3 Times 
+    # # Always Start at Battle Tab 
+    # go_battle_tab = GoToBattleTabAction()
+    # # # Treasure Hunt 3 Times 
     # treasure_hunt = TreasureHuntAction()
     # go_battle_tab = GoToBattleTabAction()
-    # Go Explore Tab
+    # # Go Explore Tab
     go_explore_tab = GoToExploreTabAction()
-    # Go Biography
-    go_biography = GoToBiographyLandmark()
+    # # Go Biography
+    # go_biography = GoToBiographyLandmark()
     # Do Panan
-    pan_an_biography = DoBioGraphyPanAn()
+    # pan_an_biography = DoBioGraphyPanAn()
+    go_errand = GoToErrandLandmark() 
+    do_errand = DoErrand()
+
    
 if __name__ == "__main__":
     main()
